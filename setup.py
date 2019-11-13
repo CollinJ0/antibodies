8a75c27dd9e14db6f5f2036c27ffe0debd90ddc1
CollinJ0 <collinjjjj3@gmail.com>
2019-11-13 10:13:54 -0800

    v1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='antibodies',
-    version='0',
+    version='1',
     #packages=['antibodies',],
     description='Helpful Scripts for Antibody NGS Data Processing',
     author="CollinJ0",
